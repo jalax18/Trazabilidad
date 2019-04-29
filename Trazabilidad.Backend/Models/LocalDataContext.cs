8eb2d6858644d48a56a8cc1d647bb7c21f6d6352
Ibz & Lax <diegog252@hotmail.com>
2019-04-29 17:29:30 +0200

    create controller users, usertypes and create new use jalax y jalax18

--- Trazabilidad.Backend/Models/LocalDataContext.cs
+++ Trazabilidad.Backend/Models/LocalDataContext.cs
@@ -7,5 +7,8 @@ namespace Trazabilidad.Backend.Models
 
     public class LocalDataContext:DataContext
     {
+        public System.Data.Entity.DbSet<Trazabilidad.Common.Models.User> Users { get; set; }
+
+        public System.Data.Entity.DbSet<Trazabilidad.Common.Models.UserType> UserTypes { get; set; }
     }
 }
