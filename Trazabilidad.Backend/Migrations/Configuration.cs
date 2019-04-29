b45cf66ad5272f0c1b21a6bab0b8dd0baba5cf21
Ibz & Lax <diegog252@hotmail.com>
2019-04-29 21:50:41 +0200

    push @class="btn btn-xxxxx" bootstrap button

---
+++ Trazabilidad.Backend/Migrations/Configuration.cs
@@ -0,0 +1,25 @@
+namespace Trazabilidad.Backend.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Trazabilidad.Backend.Models.LocalDataContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+            ContextKey = "Trazabilidad.Backend.Models.LocalDataContext";
+        }
+
+        protected override void Seed(Trazabilidad.Backend.Models.LocalDataContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
