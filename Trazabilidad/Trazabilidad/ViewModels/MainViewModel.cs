f8de46e807df710c39c94040cee8df5b769e7670
Ibz & Lax <diegog252@hotmail.com>
2019-04-30 17:36:11 +0200

    init trazabilidad Mobile

---
+++ Trazabilidad/Trazabilidad/ViewModels/MainViewModel.cs
@@ -0,0 +1,64 @@
+﻿namespace Trazabilidad.ViewModels
+{
+    using System.Collections.Generic;
+    using Models;
+
+    public class MainViewModel
+    {
+     /*   #region Properties
+        public List<Land> LandsList
+        {
+            get;
+            set;
+        }
+
+        public TokenResponse Token
+        {
+            get;
+            set;
+        }
+        #endregion*/
+
+        #region ViewModels
+        public LoginViewModel Login
+        {
+            get;
+            set;
+        }
+
+      /*  public LandsViewModel Lands
+        {
+            get;
+            set;
+        }
+
+        public LandViewModel Land
+        {
+            get;
+            set;
+        }*/
+        #endregion
+
+        #region Constructors
+        public MainViewModel()
+        {
+            instance = this;
+            this.Login = new LoginViewModel();
+        }
+        #endregion
+
+        #region Singleton
+        private static MainViewModel instance;
+
+        public static MainViewModel GetInstance()
+        {
+            if (instance == null)
+            {
+                return new MainViewModel();
+            }
+
+            return instance;
+        }
+        #endregion
+    }
+}
